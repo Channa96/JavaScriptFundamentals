8dbc0d4e722ad32e1145d7e9caa23a45736fdf7c
Channa96 <channa.kumara255@gmail.com>
2025-08-12 23:53:24 +0530

    First Commit-Variables

---
+++ basics1.js
@@ -0,0 +1,18 @@
+//printing a value
+console.log("Hello World")
+
+//Create variable and print the variable value
+let a =4
+var b = "Channa"
+const c = 10.5
+let d= true
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(!d)
+
+//Check the variable data types
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
